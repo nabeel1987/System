2a5c29d3c3f131151e7d0614084358fed9b2e4f9
nabeel1987 <nabeel_anjum@outlook.com>
2018-10-06 18:01:54 +0500

    Remove mobile

--- Customer/models.py
+++ Customer/models.py
@@ -2,6 +2,6 @@ from django.db import models
 # Create your models here.
 class Profile(models.Model):
     name=models.CharField(max_length=240, blank=False)
-    mobile_number=models.IntegerField()
+    #mobile_number=models.CharField(max_length=20,blank=False)
     def __str__(self):
         return self.name
